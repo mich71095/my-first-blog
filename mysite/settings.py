a7f74f44d3e767c956c0793cfea10d76d263eb64
mi_kill <michaelpsy71095@gmail.com>
2017-07-26 09:20:16 -0700

    additional files and changes for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,21 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+#django tutorial in heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
