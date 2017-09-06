0ae8c5d75f1d75a8f551bf787990a45f5bc405b6
michael <michaelpsy71095@gmail.com>
2017-09-06 12:55:28 -0400

    heroku

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Profile
 
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -8,5 +8,6 @@ class SomeModelAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 #admin.site.register(Post)
+admin.site.register(Profile)
 admin.site.register(Comment)
 admin.site.register(Post, SomeModelAdmin)
