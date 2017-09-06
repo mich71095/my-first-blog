9cff1b2083774d4f89279bba5b1233832e87a83d
michael <michaelpsy71095@gmail.com>
2017-09-06 14:53:52 -0400

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
